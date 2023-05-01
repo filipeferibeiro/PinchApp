c2a818c599286e6da7b625b0d4afb121d7b08119
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-05-01 15:44:51 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Filipe Fernandes on 01/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
